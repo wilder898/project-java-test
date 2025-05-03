218e08f2592054beede4ac2cd5e26093c10543eb
wilder898 <wilder898@gmail.com>
2025-05-02 20:56:51 -0500

    Modificar su programa Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Welcome to Java programming.");
+        scanner.close();
     }
 }
