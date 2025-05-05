d22bebcfdaa3e1cdac8d40bd6095191316b1b2da
wilder898 <wilder898@gmail.com>
2025-05-05 15:37:16 -0500

    Trabajar con números enteros

---
+++ src/IntegerDemo.java
@@ -0,0 +1,7 @@
+public class IntegerDemo {
+    public static void main(String[] args) {
+        // TODO: Declare an integer variable and print it
+        int number = 42;
+        System.out.println("The number is: " + number);
+    }
+}
