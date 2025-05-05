4ddabe844ab22e85ef5b5714f68d0394d87492fd
wilder898 <wilder898@gmail.com>
2025-05-05 16:02:26 -0500

    Trabajar con números de punto flotante

---
+++ src/FloatDemo.java
@@ -0,0 +1,17 @@
+public class FloatDemo {
+
+    public static void main(String[] args) {
+        // TODO: Declare and use floating-point numbers
+        double pi = 3.14159;
+        System.out.println("The value of pi is approximately: " + pi);
+
+        double radius = 5.0;
+        double area = pi * radius * radius;
+        double result = 0.1 + 0.2; // Demonstrating precision
+        System.out.println("The radius of the circle is: " + radius);
+        System.out.println("The area of the circle is: " + area);
+
+        // Demonstrating precisiondouble result = 0.1 + 0.2;
+        System.out.println("0.1 + 0.2 = " + result);
+    }
+}
